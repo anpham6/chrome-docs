3bbf6a2131b0bfdf79eb3afde3fac9c8c5ef6863
An Pham <anpham6@gmail.com>
2024-12-07 13:13:05 -0800

    Places 003: Osaka, Japan

--- source/conf.py
+++ source/conf.py
@@ -38,14 +38,15 @@ rst_prolog = """
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-  'style_nav_header_background': 'url(https://e-mc.readthedocs.io/en/latest/_static/places/002.png);',
+  'style_nav_header_background': 'url(https://e-mc.readthedocs.io/en/latest/_static/places/003.png);',
   'navigation_depth': 3,
   'includehidden': False,
 }
 html_static_path = ['_static']
 html_extra_path = [
   '_static/places/001.png',
-  '_static/places/002.png'
+  '_static/places/002.png',
+  '_static/places/003.png'
 ]
 html_css_files = ['role.css', 'content.css', 'highlight-abap.css', 'override.css']
 html_context = {

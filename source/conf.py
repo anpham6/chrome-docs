1346cacf447e2c631729a896d8ffbd36d0217972
An Pham <anpham6@gmail.com>
2025-01-07 06:51:48 -0800

    Publish: chrome-docs @ 0.5.1 [e-mc 0.11.2] [pi-r 0.9.0]

--- source/conf.py
+++ source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'E-mc'
-copyright = 'squared 2024'
+copyright = 'squared 2025'
 author = 'An Pham'
-release = '0.11.1'
+release = '0.11.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

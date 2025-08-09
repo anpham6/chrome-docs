d00e9979fb2cb36f432d6997d8003589cf8bb024
An Pham <anpham6@gmail.com>
2025-08-09 14:52:53 -0700

    Publish: chrome-docs @ 0.6.3 (e-mc 0.12.5)

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'E-mc'
 copyright = 'squared 2025'
 author = 'An Pham'
-release = '0.12.4'
+release = '0.12.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -38,7 +38,7 @@ rst_prolog = """
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-  'style_nav_header_background': 'url(https://e-mc.readthedocs.io/en/latest/_static/places/004.png);',
+  'style_nav_header_background': 'url(https://e-mc.readthedocs.io/en/latest/_static/places/005.png);',
   'navigation_depth': 3,
   'includehidden': False,
 }
@@ -47,7 +47,8 @@ html_extra_path = [
   '_static/places/001.png',
   '_static/places/002.png',
   '_static/places/003.png',
-  '_static/places/004.png'
+  '_static/places/004.png',
+  '_static/places/005.png'
 ]
 html_css_files = ['role.css', 'content.css', 'highlight-abap.css', 'override.css']
 html_context = {
